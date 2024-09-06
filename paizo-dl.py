638b8efde078c29e27a94bab32097d9c77306290
Hans-Petter Fjeld <atluxity@1kb.no>
2024-09-07 00:20:29 +0200

    sessionID example in COOKIES

--- paizo-dl.py
+++ paizo-dl.py
@@ -33,6 +33,7 @@ HEADERS = {
 
 # Cookies extracted from the curl command
 COOKIES = {
+    'sessionId': 'CHANGE ME',  # Replace with your actual sessionId from the browser, look in the web dev tool
     '_pk_id.1.638a': '156ac78b02e09b8e.1725558046.',
     '_pk_ses.1.638a': '1'
 }
